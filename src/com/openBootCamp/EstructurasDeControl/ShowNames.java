ec45bad0916673b83fdbc0ed4089571228d2ba0a
Emanuel Carballal <ema25met@gmail.com>
2022-09-01 15:57:08 -0300

    Mejora de la resolución del ejercicio.

--- src/com/openBootCamp/EstructurasDeControl/ShowNames.java
+++ src/com/openBootCamp/EstructurasDeControl/ShowNames.java
@@ -4,19 +4,60 @@ public class ShowNames {
     public static void main(String[] args) {
 
         nombresConcatenados();
-
+        nombresConcatenados2();
+        nombresConcatenados3();
     }
 
 
     //Obtiene los nombres concatenados en una linea de texto
+    //forma 1
     static void nombresConcatenados() {
 
         String[] nombres = new String[]{"Jose", "Daniel", "Ramiro", "Natalia", "Maria", "Mariano"};
 
+
+
+        StringBuilder nombresJuntos= new StringBuilder();
+
         for (String nombre : nombres) {// recorro el array de nombres.
 
-            System.out.println("Hola soy " + nombre + " mucho gusto.");
+            nombresJuntos.append(nombre).append(" ");
 
         }
+        System.out.println(nombresJuntos);
+    }
+
+    //forma2
+    static void nombresConcatenados2() {
+
+        String[] nombres2 = new String[]{"Natalia", "Mariano"};
+
+
+        String nombresJuntos2="";
+
+        for (String nombre2 : nombres2) {// recorro el array de nombres.
+
+            nombresJuntos2=nombresJuntos2.concat(nombre2+ " ");
+
+
+        }
+        System.out.println(nombresJuntos2);
+    }
+
+    //forma 3 para concatenar
+    static void nombresConcatenados3() {
+
+        String[] nombres2 ={"Natalia", "Maria", "Mariano"};
+
+
+        String nombresJuntos2="";
+
+        for (String nombre2 : nombres2) {// recorro el array de nombres.
+
+            nombresJuntos2=nombresJuntos2+ " " +nombre2;
+
+
+        }
+        System.out.println(nombresJuntos2);
     }
 }
